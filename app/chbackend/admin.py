d94585d5871c5fa7f434c9fe053fd5c40f022771
imloreno <work@imloreno.com>
2024-10-11 02:45:14 -0400

    Temporary migrations

--- app/chbackend/admin.py
+++ app/chbackend/admin.py
@@ -1,3 +1,23 @@
-from django.contrib import admin
+from django.contrib.gis import admin
+from .models import Category, City, Place
 
 # Register your models here.
+admin.site.register(Category)
+
+# Creating interface
+class CustomGeoAdmin(admin.GISModelAdmin):
+    gis_widget_kwargs = {
+        'attrs': {
+            'default_zoom': 11,
+            'default_lon': 133.74,
+            'default_lat': -24.06
+        }
+    }
+
+@admin.register(Place)
+class PlaceAdmin(CustomGeoAdmin):
+    pass
+
+@admin.register(City)
+class PlaceAdmin(CustomGeoAdmin):
+    pass
